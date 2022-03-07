90dfca9f8843bb1b699f4d68fa96561d471e8d79
FiireWiinter <andrejpopov2003@gmail.com>
2022-03-07 20:10:05 +0100

    stop placement of coins

--- src/main/java/xyz/fiire/onlysmp/listeners/coins/CoinsOnPlace.java
+++ src/main/java/xyz/fiire/onlysmp/listeners/coins/CoinsOnPlace.java
@@ -1,6 +1,7 @@
 package xyz.fiire.onlysmp.listeners.coins;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -24,7 +25,17 @@ public class CoinsOnPlace implements Listener {
         Player p = e.getPlayer();
         Block b = e.getBlock();
         if (Utils.isValidCoinBlock(b.getType())) {
-            SQLite.setLoc(b.getLocation().toString()); // add to db to detect placed blocks
+            SQLite.setLoc(b.getLocation().toString()); // add to db to detect placed block
+        } else if (p.getInventory().getItemInMainHand().getType() == Material.SUNFLOWER) {
+            if (!p.getInventory().getItemInMainHand().getItemMeta().hasCustomModelData()) return;
+            if (p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == 10) {
+                e.setCancelled(true);
+            }
+        } else if (p.getInventory().getItemInOffHand().getType() == Material.SUNFLOWER) {
+            if (!p.getInventory().getItemInOffHand().getItemMeta().hasCustomModelData()) return;
+            if (p.getInventory().getItemInOffHand().getItemMeta().getCustomModelData() == 10) {
+                e.setCancelled(true);
+            }
         }
     }
 }
